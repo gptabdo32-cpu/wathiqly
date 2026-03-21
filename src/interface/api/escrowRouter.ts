7fc36ad91c1a218ce80e02f0a541f6ed5faad93c
gptabdo32-cpu <225957692+gptabdo32-cpu@users.noreply.github.com>
2026-03-21 00:16:21 -0400

    refactor: remove redundant orchestration layers and update API router to use use-cases directly

--- src/interface/api/escrowRouter.ts
+++ src/interface/api/escrowRouter.ts
@@ -2,13 +2,28 @@ import { z } from "zod";
 import { protectedProcedure, adminProcedure, router } from "../trpc/trpc";
 import { createEscrowSchema } from "./schemas/createEscrow";
 import { openDisputeSchema, resolveDisputeSchema } from "./schemas/dispute";
-import { EscrowEngine } from "../../modules/escrow/EscrowEngine";
+import { CreateEscrow } from "../../modules/escrow/application/use-cases/CreateEscrow";
+import { ReleaseEscrow } from "../../modules/escrow/application/use-cases/ReleaseEscrow";
+import { OpenDispute, ResolveDispute } from "../../modules/escrow/application/use-cases/DisputeUseCases";
+import { DrizzleEscrowRepository } from "../../modules/escrow/infrastructure/DrizzleEscrowRepository";
+import { PaymentService } from "../../modules/escrow/infrastructure/PaymentService";
+import { LedgerService } from "../../modules/blockchain/LedgerService";
+
+// Helper to get dependencies (Temporary until DI container is implemented)
+const getEscrowDependencies = () => {
+  const ledgerService = new LedgerService();
+  const paymentService = new PaymentService(ledgerService);
+  const escrowRepo = new DrizzleEscrowRepository();
+  return { paymentService, escrowRepo };
+};
 
 export const escrowRouter = router({
   create: protectedProcedure
     .input(createEscrowSchema)
     .mutation(async ({ ctx, input }) => {
-      const escrowId = await EscrowEngine.lockFunds({
+      const { paymentService, escrowRepo } = getEscrowDependencies();
+      const useCase = new CreateEscrow(paymentService, escrowRepo);
+      const escrowId = await useCase.execute({
         buyerId: ctx.user.id,
         sellerId: input.sellerId,
         amount: input.amount,
@@ -21,14 +36,18 @@ export const escrowRouter = router({
   release: protectedProcedure
     .input(z.object({ escrowId: z.number().int().positive() }))
     .mutation(async ({ input }) => {
-      const success = await EscrowEngine.releaseFunds(input.escrowId);
+      const { paymentService, escrowRepo } = getEscrowDependencies();
+      const useCase = new ReleaseEscrow(paymentService, escrowRepo);
+      const success = await useCase.execute(input.escrowId);
       return { success };
     }),
 
   openDispute: protectedProcedure
     .input(openDisputeSchema)
     .mutation(async ({ input }) => {
-      const disputeId = await EscrowEngine.openDispute(
+      const { escrowRepo } = getEscrowDependencies();
+      const useCase = new OpenDispute(escrowRepo);
+      const disputeId = await useCase.execute(
         input.escrowId,
         input.initiatorId,
         input.reason
@@ -39,7 +58,9 @@ export const escrowRouter = router({
   resolveDispute: adminProcedure
     .input(resolveDisputeSchema)
     .mutation(async ({ input }) => {
-      const success = await EscrowEngine.resolveDispute(
+      const { paymentService, escrowRepo } = getEscrowDependencies();
+      const useCase = new ResolveDispute(paymentService, escrowRepo);
+      const success = await useCase.execute(
         input.disputeId,
         input.adminId,
         input.resolution
